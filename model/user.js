b52cac5a23cf448e1f8a0405ad320044de9291b7
hamid <zare.hamid4754@gmail.com>
2022-05-29 17:27:11 +0430

    complete user Schemam

--- model/user.js
+++ model/user.js
@@ -1,14 +1,50 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
     type: Number,
     required: true,
   },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
+  },
+  zip:{
+    type: String,
+    default: ''
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-// const User = mongoose.model("User", userSchema);
-// module.exports = User;
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+userSchema.set("toJSON", { virtuals: true });
 
 exports.User = mongoose.model("User", userSchema);
