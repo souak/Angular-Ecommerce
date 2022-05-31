845507011a626ad760d32cb7f76e06780a4a8bbf
hamid <zare.hamid4754@gmail.com>
2022-05-31 21:41:49 +0430

    add revoke method

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,13 +1,28 @@
-const { expressjwt } = require("express-jwt");
+const expressjwt = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.SECRET;
+  const api = process.env.URL_VERSION;
   return expressjwt({
     secret,
     algorithms: ["HS256"],
+    isRevoked: isRevoked,
   }).unless({
-    path: ["/api/v1/users/login"],              //exclude login path from authorization
+    path: [
+      `${api}/users/login`, //exclude login path from authorization
+      `${api}/users/register`,
+      { url: /\/api\/v1\/products(.*)/, method: ["GET", "OPTIONS"] }, //exclude all get methods after /api/v1/products/...   from authorization
+      { url: /\/api\/v1\/categories(.*)/, method: ["GET", "OPTIONS"] },
+    ],
   });
 }
+
+const isRevoked = async (req, payload, done) => {   //rovoke apis that are not from admins (can't do DELETE POST PUT)
+  if (!payload.isAdmin) {
+   return done(null, true)
+  }
+  return done()
+
+};
 
 module.exports = authJwt;
